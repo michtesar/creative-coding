680e872a01535514464397045d32537d6ef202e4
Michael Tesar <michael_tesar@icloud.com>
2025-11-27 15:20:15 +0100

    Refactor layout to improve readability and ensure proper integration of SpeedInsights component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,7 +26,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
 				{children}
 				<SpeedInsights />
 				<Analytics />
